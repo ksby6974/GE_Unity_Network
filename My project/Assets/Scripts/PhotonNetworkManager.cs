f2934e38f86da47f21062c5da1e1f4afb7c178fc
ksby6974 <yswtkddn6479@naver.com>
2024-10-16 15:41:03 +0900

    241016
    .

--- My project/Assets/Scripts/PhotonNetworkManager.cs
+++ My project/Assets/Scripts/PhotonNetworkManager.cs
@@ -3,28 +3,63 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
+using PlayFab;
+using PlayFab.ClientModels;
 
 public class PhotonNetworkManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] InputField emailInputField;
     [SerializeField] InputField passwordInputField;
 
-    public void Success()
+    public void Success(LoginResult loginResult)
     {
         PhotonNetwork.AutomaticallySyncScene = false;
         PhotonNetwork.GameVersion = "1.0f";
         PhotonNetwork.LoadLevel("LobbyScene");
     }
 
-    // Start is called before the first frame update
-    void Start()
+    public void Success(RegisterPlayFabUserResult registerPlayFabUserResult)
     {
-        
+        Debug.Log(registerPlayFabUserResult.ToString());
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Failure(PlayFabError playFaberror)
     {
-        
+        Debug.Log(playFaberror.ToString());
+    }
+
+    public void OnSignUp()
+    {
+        var result = new RegisterPlayFabUserRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text,
+            RequireBothUsernameAndEmail = false
+        };
+
+        PlayFabClientAPI.RegisterPlayFabUser
+        (
+            result,
+            Success,
+            Failure
+        );
+    }
+
+    public void OnSignIn()
+    {
+        var request = new LoginWithEmailAddressRequest
+        {
+            Email = emailInputField.text,
+            Password = passwordInputField.text
+        };
+
+
+        PlayFabClientAPI.LoginWithEmailAddress
+        (
+            request,
+            Success,
+            Failure
+        );
     }
 }
+
---
+++ My project/Assets/Scripts/PopUpManager.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopUpManager : MonoBehaviour
+{
+    [SerializeField] List<GameObject> popUpList;
+
+    private static PopUpManager instance;
+
+    public static PopUpManager Instance
+    {
+        get { return instance; }
+    }
+
+    public void Awake()
+    {
+        popUpList.Capacity = 10;
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    public void Create(string popUpName)
+    {
+        for (int i = 0; i < popUpList.Count; i++)
+        {
+            if (popUpName == popUpList[i].name)
+            {
+                popUpList[i].gameObject.SetActive(true);
+                return;
+            }
+        }
+
+        popUpList.Add(Instantiate(Resources.Load<GameObject>(popUpName), gameObject.transform.GetChild(0)));
+    }
+}
